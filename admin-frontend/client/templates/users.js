dc82aa5afd433e4fa425e015687a41f63b1b192d
ariel.lev <ariel.lev@valtech.de>
2016-05-26 01:40:12 +0200

    adding controllers

--- admin-frontend/client/templates/users.js
+++ admin-frontend/client/templates/users.js
@@ -3,3 +3,32 @@ Template.users.helpers({
     return Users.find();
   }
 });
+
+Template.users.events({
+    'submit form': function(e, template) {
+        e.preventDefault();
+        console.log('Post saved');
+        var form = e.target;
+        var user = Meteor.user();
+        var slug = _.slugify(form.title.value);
+        var _this = this;
+
+        // calling method
+        Meteor.call('insertPost', {
+          title: form.title.value,
+          slug: slug,
+          description: form.description.value,
+          text: form.text.value
+        }, function(error, slug) {
+
+          Session.set('saveButton', 'Save post');
+
+          if (error) {
+              alert(error.reason);
+          } else {
+              // redirect
+              Router.go('Post', {slug: slug});
+          }
+        });
+    }
+});
