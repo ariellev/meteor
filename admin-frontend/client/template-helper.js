9e34d698ecc3270f16e1eaf8afa0304aa495721f
ariel.lev <ariel.lev@valtech.de>
2016-05-27 13:32:22 +0200

    clean up

--- admin-frontend/client/template-helper.js
+++
@@ -1,8 +0,0 @@
-Template.registerHelper('formatTime', function(time,type) {
-
-  switch (type) {
-    case 'fromNow': return moment.utc(time).fromNow();
-    case 'iso': return moment.utc(time).toISOString();
-    default: return moment.utc(time).format('LL');
-  }
-});
